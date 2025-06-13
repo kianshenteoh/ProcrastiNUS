cf532574fa7949d13c257e2e0b8a346787006687
Danton <dantonyap2016@gmail.com>
2025-06-14 01:17:38 +0800

    Placed log out button in Settings

--- app/(tabs)/social.js
+++ app/(tabs)/social.js
@@ -1,11 +1,9 @@
 import { Image, View } from 'react-native';
-import TopBar from '../../components/ui/TopBar.js';
 
 export default function social() {
     
     return (<>
     <View style = {{flex: 1}}> 
-    <TopBar />
     <View style = {{flex: 1}}>
         <Image
         source = {require('../../assets/images/friends-image.png')}
