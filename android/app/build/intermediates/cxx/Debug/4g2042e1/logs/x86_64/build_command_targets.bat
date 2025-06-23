b8d3f8d0aec8d9d94f2f5e47ee52e4b4ac16ae56
kianshenteoh <kianshen12@gmail.com>
2025-06-23 19:04:51 +0800

    Remove android folder

--- android/app/build/intermediates/cxx/Debug/4g2042e1/logs/x86_64/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\kians\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\kians\\ProcrastiNUS\\android\\app\\.cxx\\Debug\\4g2042e1\\x86_64" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
